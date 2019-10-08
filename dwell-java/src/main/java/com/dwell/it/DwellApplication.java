2177ad001a26910f9810260019113dc046b38e23
toureek <june.key.young@gmail.com>
2019-10-08 10:25:24 +0800

    Setup SpringBoot project from 'https://start.spring.io'

---
+++ dwell-java/src/main/java/com/dwell/it/DwellApplication.java
@@ -0,0 +1,13 @@
+package com.dwell.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwellApplication.class, args);
+	}
+
+}
